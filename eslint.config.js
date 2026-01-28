e872e049283d7b0892fb1690d3f225d781b4f376
Shoya Sakai <heppoko0810@gmail.com>
2026-01-28 22:13:26 +0900

    chore: update deps and ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+const nodeGlobals = { ...globals.node };
+delete nodeGlobals.crypto;
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script",
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["src/renderer/**/*.js"],
+    ignores: ["src/renderer/**/*.test.js", "src/renderer/**/*.spec.js"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: [
+      "src/main/**/*.js",
+      "src/preload/**/*.js",
+      "src/renderer/**/*.test.js",
+      "src/renderer/**/*.spec.js",
+      "scripts/**/*.js",
+    ],
+    languageOptions: {
+      globals: nodeGlobals,
+    },
+  },
+];
